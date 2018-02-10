c233ba5f8e6ee00bb4e75fdcb6c854eb4eb1c0f0
Wildan Mubarok <willnode@wellosoft.net>
2018-02-11 06:10:12 +0700

    add readme
    + spaces for tab
    + little disclaimer

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # uas-public-stat
+
+(Open the tool here)[https://wellosoft.net/uas-public-stat]
+
+This is a tool to leverage public statistics for Asset store packages for given publisher id.
+
+It does work by fetching internal asset store API then use (cors-anywhere)[https://cors-anywhere.herukoapp.com] to bypass CORS.
+
+Few examples of public statistics that exposed in this tool are like package ID, exact update date, hotness/populariy score, etc. There also icons that when clicked, will opening a new tab to asset store page of clicked asset.
+
+Configuration will be saved in `localStorage` while responses are cached in `sessionStorage`. I don't put cookies or analytics in this tool.
+
+## Disclaimer
+
+This is an unnoficial tool. Feel free to fill an issue in issues tab.
+
+[Also see here](main.js#L18)
--- main.js
+++ main.js
@@ -4,50 +4,55 @@ const showPublisherRegex = /^\d+/;
 const shouldShowPublisher = function (s) { return !showPublisherRegex.test(s); }
 
 var data = {
-    data: JSON.parse(sessionStorage['uas-public-data'] || '{"total": 0, "results":[] }'),
-    page: 1,
-    pageMax: 1,
-    pubId: localStorage['uas-id'],
-    order: localStorage['uas-order'] || 'popularity',
-    orderChoices: ['popularity', 'name', 'price', 'rating', 'updated'],
-    message: '',
-    showPublishers: shouldShowPublisher(localStorage['uas-id']),
-    request: function () {
-        var url = `${apiUrl}?q=publisher%3A${(data.pubId + "").replace(',', '&q=')
-            }&q=type:content&rows=${pageMaxItems}&page=${data.page}&order_by=${data.order}`
+	data: JSON.parse(sessionStorage['uas-public-data'] || '{"total": 0, "results":[] }'),
+	page: 1,
+	pageMax: 1,
+	pubId: localStorage['uas-id'],
+	order: localStorage['uas-order'] || 'popularity',
+	orderChoices: ['popularity', 'name', 'price', 'rating', 'updated'],
+	message: '',
+	showPublishers: shouldShowPublisher(localStorage['uas-id']),
+	request: function () {
 
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
-            if (this.readyState == 4) {
-                if (this.status == 200) {
-                    data.data = JSON.parse(sessionStorage['uas-public-data'] = this.responseText);
-                    data.pageMax = Math.ceil(data.data.total / pageMaxItems) || 1;
-                    localStorage['uas-id'] = data.pubId;
-                    localStorage['uas-order'] = data.order;
-                    data.showPublishers = shouldShowPublisher(data.pubId);
-                    data.message = '';
-                } else {
-                    data.message = `⛔ Failed to receive request (${this.status})`;
-                }
-            } else {
-                data.message = '🔃 Sending request...';
-            }
-        };
-        xhr.ontimeout = function () {
-            data.message = `⛔ Request timed out`;
-        }
-        xhr.open("GET", url, true);
-        xhr.timeout = 10000; // Sometimes it does not reply at all.
-        xhr.setRequestHeader("accept", "application/json")
-        xhr.send();
-    }
+		// Turns out from this single API I can leverage other powerful search query here.
+		// This also means that this tool can be a light alternative for searching assets.
+		// Unfornatunely, I haven't check the EULA for that :/
+		// Anyway I will make this easier by assuming comma for separated query.
+		var url = `${apiUrl}?q=publisher%3A${(data.pubId + "").replace(',', '&q=')
+			}&q=type:content&rows=${pageMaxItems}&page=${data.page}&order_by=${data.order}`
+
+		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function () {
+			if (this.readyState == 4) {
+				if (this.status == 200) {
+					data.data = JSON.parse(sessionStorage['uas-public-data'] = this.responseText);
+					data.pageMax = Math.ceil(data.data.total / pageMaxItems) || 1;
+					localStorage['uas-id'] = data.pubId;
+					localStorage['uas-order'] = data.order;
+					data.showPublishers = shouldShowPublisher(data.pubId);
+					data.message = '';
+				} else {
+					data.message = `⛔ Failed to receive request (${this.status})`;
+				}
+			} else {
+				data.message = '🔃 Sending request...';
+			}
+		};
+		xhr.ontimeout = function () {
+			data.message = `⛔ Request timed out`;
+		}
+		xhr.open("GET", url, true);
+		xhr.timeout = 10000; // Sometimes it does not reply at all.
+		xhr.setRequestHeader("accept", "application/json")
+		xhr.send();
+	}
 };
 
 data.pageMax = Math.ceil(data.data.total / pageMaxItems) || 1;
 
 (() => {
-    new Vue({
-        el: "#app",
-        data: data
-    });
+	new Vue({
+		el: "#app",
+		data: data
+	});
 })();
