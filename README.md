2a200197010a6b7c444c93d2527c99bc5be96766
Wildan Mubarok <willnode@wellosoft.net>
2018-02-12 13:06:17 +0700

    🔥 hotness diff
    and fix bugs (have you see the readme 😸)
    and add link to repo

--- README.md
+++ README.md
@@ -1,17 +1,27 @@
 # uas-public-stat
 
-(Open the tool here)[https://wellosoft.net/uas-public-stat]
+[Open the tool here](https://wellosoft.net/uas-public-stat)
 
-This is a tool to leverage public statistics for Asset store packages for given publisher id.
+This is a tool to leverage public statistics for Asset store packages for given [publisher ID](https://i.imgur.com/uBFYQMA.png).
 
-It does work by fetching internal asset store API then use (cors-anywhere)[https://cors-anywhere.herukoapp.com] to bypass CORS.
+It does work by fetching internal asset store API then using [cors-anywhere](https://cors-anywhere.herukoapp.com) to bypass CORS restriction.
 
 Few examples of public statistics that exposed in this tool are like package ID, exact update date, hotness/populariy score, etc. There also icons that when clicked, will opening a new tab to asset store page of clicked asset.
 
 Configuration will be saved in `localStorage` while responses are cached in `sessionStorage`. I don't put cookies or analytics in this tool.
+
+## Hotness Difference
+
+This tool also tracks the hotness of every package that you looking with this tool. Tracking works by caching the initial hotness the first time it appeared and calculate the difference with current hotness value. The cache is saved in `localStorage` so it doesn't expire.
+
+You can also clear that cache by clicking `Reset Hotness Difference` in the bottom of page. It also worth to note that UT usually change the hotness rating every 1-2 hour even though it's a darn pretty small difference.
 
 ## Disclaimer
 
 This is an unnoficial tool. Feel free to fill an issue in issues tab.
 
 [Also see here](main.js#L18)
+
+## Related
+
+[I also made another similar tool](https://github.com/willnode/uas-private-stat/)
--- main.js
+++ main.js
@@ -7,25 +7,34 @@ var data = {
 	data: JSON.parse(sessionStorage['uas-public-data'] || '{"total": 0, "results":[] }'),
 	page: 1,
 	pageMax: 1,
-	pubId: localStorage['uas-id'],
+	pubId: localStorage['uas-id'] || '',
 	order: localStorage['uas-order'] || 'popularity',
+	hotnessDB: JSON.parse(localStorage['uas-public-hotness'] || "{}"),
 	orderChoices: ['popularity', 'name', 'price', 'rating', 'updated'],
 	message: '',
 	showPublishers: shouldShowPublisher(localStorage['uas-id']),
+	resetHotnessDB: function () { data.hotnessDB = {}; localStorage.removeItem('uas-public-hotness'); },
 	request: function () {
 
 		// Turns out from this single API I can leverage other powerful search query here.
 		// This also means that this tool can be a light alternative for searching assets.
 		// Unfornatunely, I haven't check the EULA for that :/
 		// Anyway I will make this easier by assuming comma for separated query.
-		var url = `${apiUrl}?q=publisher%3A${(data.pubId + "").replace(',', '&q=')
-			}&q=type:content&rows=${pageMaxItems}&page=${data.page}&order_by=${data.order}`
+		var url = `${apiUrl}?q=publisher%3A${(data.pubId + "").replace(/,/g, '&q=')
+			}&q=type:content&rows=${pageMaxItems}&page=${data.page}&order_by=${data.order}`;
 
 		var xhr = new XMLHttpRequest();
 		xhr.onreadystatechange = function () {
 			if (this.readyState == 4) {
 				if (this.status == 200) {
-					data.data = JSON.parse(sessionStorage['uas-public-data'] = this.responseText);
+					var d = JSON.parse(sessionStorage['uas-public-data'] = this.responseText);
+					var hDB = data.hotnessDB;
+					d.results.forEach(el => {
+						if (hDB[el.id] === undefined)
+							hDB[el.id] = el.hotness;
+					});
+					localStorage['uas-public-hotness'] = JSON.stringify(data.hotnessDB = hDB);
+					data.data = d;
 					data.pageMax = Math.ceil(data.data.total / pageMaxItems) || 1;
 					localStorage['uas-id'] = data.pubId;
 					localStorage['uas-order'] = data.order;
